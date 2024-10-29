ee9c28156ab26564b967b91c603b6bea69939870
gilzafire <gilza.ab@gmail.com>
2024-10-29 16:09:08 +0100

    good architecture principe change

--- host/webserver/app.py
+++ host/webserver/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from the Web Server!"
+def hello():
+    return "Hello from the web server behind the VPN!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
